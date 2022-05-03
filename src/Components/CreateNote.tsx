ce73011936cd6d90afdcc64cf91702806202c8a5
Eric Yi <erickanginyi@gmail.com>
2022-05-02 23:26:20 -0400

    Create file, add comments
    Create CreateNote.tsx file
    
    Import Note from Models folder to use related Note interface
    Import Alert & Form from react-bootstrap to use inside of component return
    Import Button from react-bootstrap/Button to use inside of component return
    
    Populate ICreateNoteProps interface with notes and setNotes
    --> notes set to an array with
        the Note interface inside
    --> setNotes set to typing of useState setter function
    
    Take in notes & setNotes as props from parent component
    
    Create errorMessage state to handle potential error messages; set typing to string & set initial state to empty string
    
    Create titleRef, textRef, and colorRef variables to handle the values from related inputs/textareas; set typing to related HTML elements|null & set initial to null
    
    Create handleSubmit function
    --> e.preventDefault to prevent page refresh upon submit
    --> If either the title value or text value are empty, setErrorMessage to text string
    --> If it gets past the empty string guard clause, then setErrorMessage to an empty string
        --> Then setNotes to a new array with a spread copy of the notes props array and the newly created note inside of it
    --> Clear both title and text inputs
    
    Create and render JSX elements

---
+++ src/Components/CreateNote.tsx
@@ -0,0 +1,86 @@
+import React, {useRef, useState} from "react";
+
+import {Note} from "../Models/note.model";
+
+import {Alert, Form} from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+
+interface ICreateNoteProps {
+   notes: Note[],
+   setNotes: React.Dispatch<React.SetStateAction<Note[]>>
+}
+
+const CreateNote:React.FC<ICreateNoteProps> = ({notes, setNotes}) => {
+   const [errorMessage, setErrorMessage] = useState<string>("");
+
+   const titleRef = useRef<HTMLInputElement|null>(null);
+   const textRef = useRef<HTMLTextAreaElement|null>(null);
+   const colorRef = useRef<HTMLInputElement|null>(null);
+
+   const handleSubmit = (e:React.FormEvent<HTMLFormElement>):void => {
+      e.preventDefault();
+
+      // Has the same effect as placing the "required" attribute on the input/textarea
+      if (titleRef.current?.value === "" || textRef.current?.value === "") {
+         return setErrorMessage("Please input all fields");
+      }
+
+      setErrorMessage("");
+      setNotes([...notes, {
+         id: (new Date().toString()),
+         // Typings for the relatedRefs
+         title: (titleRef.current as HTMLInputElement).value,
+         text: (textRef.current as HTMLTextAreaElement).value,
+         color: (colorRef.current as HTMLInputElement).value,
+         date: `Created on ${(new Date().toLocaleDateString())} at ${(new Date().toLocaleTimeString())}`
+      }]);
+
+      // Clearing the inputs
+      (titleRef.current as HTMLInputElement).value = "";
+      (textRef.current as HTMLTextAreaElement).value = "";
+   }
+
+   return (
+      <>
+         <h2>CREATE A NOTE</h2>
+
+         {errorMessage &&
+            <Alert variant="danger">
+               {errorMessage}
+            </Alert>
+         }
+
+         <Form className="mt-3 mb-3" onSubmit={handleSubmit}>
+            <Form.Group className="mb-3" controlId="formBasicTitle">
+               <Form.Label>
+                  Title
+               </Form.Label>
+
+               <Form.Control type="text" placeholder="Enter Title" ref={titleRef}/>
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicText">
+               <Form.Label>
+                  Text
+               </Form.Label>
+
+               <Form.Control placeholder="Enter Note" as="textarea" rows={3} ref={textRef}/>
+            </Form.Group>
+
+            <Form.Group className="mb-3">
+               <Form.Label htmlFor="colorInput">
+                  Background Color
+               </Form.Label>
+
+               <Form.Control type="color" id="colorInput" defaultValue="#dfdfdf" title="Choose a color" ref={colorRef}/>
+            </Form.Group>
+
+            <Button type="submit" variant="primary">
+               Submit
+            </Button>
+         </Form>
+      </>
+   );
+}
+
+export default CreateNote;
