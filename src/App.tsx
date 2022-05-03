672b70cf7cdd80252062de24be2e7114c8c6fa3d
Eric Yi <erickanginyi@gmail.com>
2022-05-02 23:56:22 -0400

    Import required things, populate file
    Import useState from react
    Import related child components; CreateNote, Header, and NotesList components
    Import Note from Models folder to use related Note interface
    Import Col, Container, and Row from react-bootstrap
    
    Create notes state to handle all notes information; set typing to Note interface array & set initial state to empty array
    
    Create and render JSX elements
    Pass down notes & setNotes as props into both NotesList and CreateNote child components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, {useState} from "react";
 
-function App() {
+import CreateNote from "./Components/CreateNote";
+import Header from "./Components/Header";
+import NotesList from "./Components/NotesList";
+
+import {Note} from "./Models/note.model";
+
+import {Col, Container, Row} from "react-bootstrap";
+
+const App = () => {
+   const [notes, setNotes] = useState<Note[]>([]);
+
    return (
       <>
-         asdf
+         <Header />
+         <Container className="margin-top">
+            <Row>
+               <Col>
+                  <NotesList notes={notes} setNotes={setNotes}/>
+               </Col>
+            </Row>
+               <hr/>
+            <Row>
+               <Col>
+                  <CreateNote notes={notes} setNotes={setNotes}/>
+               </Col>
+            </Row>
+         </Container>
       </>
    );
 }
