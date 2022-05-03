bbb1c41e80714d2e1ec8d3bb9a4869ad1e947851
Eric Yi <erickanginyi@gmail.com>
2022-05-03 14:20:08 -0400

    Create file
    Create AllNotes.tsx file
    
    Import Notes child component
    Import Note from Models folder to user related Note interface
    
    Populate IAllNotesProps with notes and setNotes
    --> note set to an array filled with Note types
    --> setNotes set to typing of useState setter function
    
    Take in notes & setNotes as props from parent component
    
    Create handleDelete function to handle deleting notes using their ids
    --> Filter through notes and remove the note with the same id as the deleted note
    
    Create renderNotes variable to hold mapped array to render
    --> Map through notes array and for each note in the arraym return and render child component
        --> Pass down related information as props into child component
    
    Create and render JSX elements
    Conditionally render different text if renderNotes.length is a falsey value (0)

---
+++ src/Components/AllNotes.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+import Notes from "./Notes";
+
+import {Note} from "../Models/note.model";
+
+interface IAllNotesProps {
+   notes: Note[],
+   setNotes: React.Dispatch<React.SetStateAction<Note[]>>
+}
+
+// React.FunctionComponent can be better shortened to be React.FC, both work interchangably
+const AllNotes:React.FC<IAllNotesProps> = ({notes, setNotes}) => {
+   const handleDelete = (id:string) => {
+      setNotes(notes.filter(note => note.id !== id));
+   }
+
+   const renderNotes:JSX.Element[] = notes?.map(note => {
+      return <Notes key={note.id} note={note} handleDelete={handleDelete}/>
+   });
+
+   return (
+      <>
+         <div className="mt-2">
+            {renderNotes.length
+               ? renderNotes
+               : <h2>There are no notes yet.</h2>
+            }
+         </div>
+      </>
+   );
+}
+
+export default AllNotes;
