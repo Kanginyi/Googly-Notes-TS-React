1d52b05730c39bb32f515807bc9d2ce5acf35a4e
Eric Yi <erickanginyi@gmail.com>
2022-05-04 03:49:29 -0400

    Import Button, update interface, create hideOrShow function, conditionally render JSX
    Import Button from react-bootstrap/Button to use inside of component return
    
    Update IAllNotesProps interface
    --> Add hideNotes key; set typing to boolean
    --> Add setHideNotes key; set typing to useState setter function as a boolean
    
    Take in hideNote & setHideNotes as props
    
    Craete hideOrShow function to handle either showing or hiding all notes on page triggered by related onClick
    
    Conditionally render all notes based on hideNotes state
    
    Add bootstrap Button to JSX elements with onClick set to hideOrShow funciton
    --> Conditionally render text inside of the button based on hideNotes state

--- src/Components/AllNotes.tsx
+++ src/Components/AllNotes.tsx
@@ -4,12 +4,20 @@ import EachNote from "./EachNote";
 
 import {Note} from "../Models/note.model";
 
+import Button from "react-bootstrap/Button";
+
 interface IAllNotesProps {
    notes: Note[],
    setNotes: React.Dispatch<React.SetStateAction<Note[]>>
+   hideNotes: boolean,
+   setHideNotes: React.Dispatch<React.SetStateAction<boolean>>
 }
 
-const AllNotes:React.FC<IAllNotesProps> = ({notes, setNotes}) => {
+const AllNotes:React.FC<IAllNotesProps> = ({notes, setNotes, hideNotes, setHideNotes}) => {
+   const hideOrShow = ():void => {
+      setHideNotes(prev => !prev);
+   };
+
    const handleDelete = (id:string) => {
       setNotes(notes.filter(note => note.id !== id));
    }
@@ -24,10 +32,21 @@ const AllNotes:React.FC<IAllNotesProps> = ({notes, setNotes}) => {
             {renderNotes.length
                ? <>
                   <h2 className="mb-3 align-center">NOTES</h2>
-                     {renderNotes.length === 1
-                        ? <div>{renderNotes}</div>
-                        : <div className="grid">{renderNotes}</div>
+
+                     {!hideNotes &&
+                        <>
+                           {renderNotes.length === 1
+                              ? <div>{renderNotes}</div>
+                              : <div className="grid">{renderNotes}</div>
+                           }
+                        </>
                      }
+
+                  <div className="align-center">
+                     <Button onClick={hideOrShow} variant="outline-primary" size="sm" className="mt-2">
+                        {!hideNotes ? "Hide Notes" : "Show Notes"}
+                     </Button>
+                  </div>
                  </>
                : <h2 className="align-center">There are no notes yet.</h2>
             }
